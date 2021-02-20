bca934c6fc76f315c589f7465b0b7d052b6bb90e
lijinxin <1851659071@qq.com>
2021-02-20 17:02:39 +0800

    新增文件Main5,添加到git控制

---
+++ src/com/company/Main5.java
@@ -0,0 +1,12 @@
+package com.company;
+
+/**
+ * @Author KAY
+ * @create {YEAR}-{MONTH}-{DAY}  {TIME}
+ */
+public class Main5 {
+
+    public static void main(String[] args) {
+        System.out.println("新建文件Main5 添加到git 版本控制");
+    }
+}
